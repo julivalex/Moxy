3f30ccf9d8bcb1c47c2d3722b22fe18a3284681a
Yulia <123>
2018-11-30 22:21:44 +0400

    init project

---
+++ example_6/src/main/java/com/kotlin/note/example_6/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kotlin.note.example_6
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
